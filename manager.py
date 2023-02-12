45eea9f66392730c7ca5765b11eed1ce918cb1c1
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2023-02-12 14:41:55 -0400

    still busy building the skeleton
    hehe skelton

--- manager.py
+++ manager.py
@@ -3,24 +3,33 @@
 from typing import Final
 from sys import argv, stderr
 
+HELP_TXT: Final = ''
+DEFAULT_STEM: Final = 'main'
+
 
 def eprint(*values: object, **kwargs: object):
     '''prints to `stderr`'''
     return print(*values, file=stderr, **kwargs)
 
 
-DEFAULT_STEM: Final = 'main'
+def print_help():
+	print(HELP_TXT)
+	return HELP_TXT
 
 
 def main(*args: str):
-	from sys import exit as sys_exit # avoid collision with global `exit`
+	from sys import exit as sys_exit  # avoid collision with global `exit`
+
+	if len(args) < 1:
+		return print_help()
 
 	match args[0]:
+		case 'help': return print_help()
 		case 'check': pass
 		case 'add': pass
 		case 'sort': pass
 		case _:
-			eprint('unrecognized subcmd')
+			eprint('unrecognized subcmd\nuse "help"')
 			sys_exit(1)
 
 
