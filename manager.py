3607fe970e8df2259c161e753bbb2f38eab6d849
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2023-02-12 14:27:31 -0400

    manager skeleton is almost ready

--- manager.py
+++ manager.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python3
 
 from typing import Final
-from sys import argv
+from sys import argv, stderr
+
+
+def eprint(*values: object, **kwargs: object):
+    '''prints to `stderr`'''
+    return print(*values, file=stderr, **kwargs)
+
 
 DEFAULT_STEM: Final = 'main'
 
 
 def main(*args: str):
-	if args[0] == 'sort':
-		pass
+	from sys import exit as sys_exit # avoid collision with global `exit`
+
+	match args[0]:
+		case 'sort': pass
+		case 'add': pass
+		case 'check': pass
+		case _:
+			eprint('unrecognized subcmd')
+			sys_exit(1)
 
 
 if '__main__' == __name__:
